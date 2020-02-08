9b3f5017404c810c25098ce40f6fafd04aabb3e1
lina-hs <lina.hannersanchez@gmail.com>
2020-02-08 17:28:12 +0100

    add endpoints to get by id, update and delete groups

--- groups/groups.data.js
+++ groups/groups.data.js
@@ -11,18 +11,55 @@ function getGroups(result) {
     });
 }
 
+function getGroupById(id, result) {
+    db.query("SELECT * FROM groups WHERE id = ?", id, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result.status(500).send(err);
+        } else {
+            result.json(res[0]);
+        }
+    });  
+}
+
 function addGroup(group, result) {
     db.query("INSERT INTO groups set ?", group, function (err, res, fields) {
         if (err) {
             console.log("error: ", err);
             result.status(500).send(err);
         } else {
-            result.json(res.insertId);
+            result.status(201).json(res.insertId);
+        }
+    });
+}
+
+function updateGroup(id, group, result) {
+    db.query("UPDATE groups SET ? WHERE id = ?", [group, id], function (err, res) {
+        if (err) {
+            console.log("error: ", err);
+            result.status(500).send(err);
+        }
+        else {   
+            result.json(res.group);
+        }
+    }); 
+}
+
+function deleteGroup(id, result) {
+    db.query("DELETE FROM groups WHERE id = ?", id, function(err, es) {
+        if (err) {
+            console.log("error: ", err);
+            result.status(500).send(err);
+        } else {
+            result.json();
         }
     });
 }
 
 module.exports = {
     getGroups,
-    addGroup
+    getGroupById,
+    addGroup,
+    updateGroup,
+    deleteGroup
 }
--- groups/groups.router.js
+++ groups/groups.router.js
@@ -3,12 +3,29 @@ var router = express.Router()
 
 const data = require('./groups.data');
 
-// Get groups
+// Get all groups
 router.get('/', (req, res) => {
     data.getGroups(res);
 });
+
+// Get group by id
+router.get('/:id', (req, res) => {
+    data.getGroupById(req.params.id, res);
+});
+
+// Create group
 router.post('/', (req, res) => {
     data.addGroup(req.body, res);
+});
+
+// Update group
+router.put('/:id', (req, res) => {
+    data.updateGroup(req.params.id, req.body, res);
+});
+
+// Delete group
+router.delete('/:id', (req, res) => {
+    data.deleteGroup(req.params.id, res);
 });
 
 module.exports = router;
