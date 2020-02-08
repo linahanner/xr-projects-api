8feae386f21f697441cc68a7ad76ea2b3889daf0
lina-hs <lina.hannersanchez@gmail.com>
2020-02-08 16:46:20 +0100

    route to groups router

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const express = require('express')
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// create database connection
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "xrprojectsdb"
+});
 
+// connect to database
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+
+global.db = db;
+
+// parse application/json
+app.use(bodyParser.json())
+
+// set app routers
+const router = require('./router');
+app.use('/', router);
+
+// set app to listen
 app.listen(port, () => console.log(`XR Projects API listening on port ${port}!`))
---
+++ router.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var router = express.Router()
+
+// routers
+const groups = require('./groups/groups.router');
+
+// middleware
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+
+// routing
+router.get('/', (req, res) => res.send("Hello, World!"));
+router.use('/groups', groups);
+
+module.exports = router;
