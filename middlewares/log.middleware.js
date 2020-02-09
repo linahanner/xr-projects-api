7ef3a82b441c0c5b36b4f34f182b979112dc9b72
Anton Strömkvist <anton@stromkvist.com>
2020-02-09 15:00:06 +0100

    Run prettier on log.middleware.js

--- middlewares/log.middleware.js
+++ middlewares/log.middleware.js
@@ -1,8 +1,8 @@
 const logger = global.logger;
 
 const log = (req, res, next) => {
-    logger.info(`${req.method}: ${req.path}`);
-    next();
-}
+  logger.info(`${req.method}: ${req.path}`);
+  next();
+};
 
 module.exports = log;
